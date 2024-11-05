a66015303dc5ba5fcb5e30af478bbc9e0682894d
Anthonin_IZORET <anthonin.i82@gmail.com>
2024-11-05 12:22:23 +0100

    k

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,8 +15,40 @@ public class PlayerController : MonoBehaviour
     public bool onRightWall;
     public bool onWall;
 
+    public bool onGround;   
+
+    public bool isJumping = false;
+    public bool reachedJumpApexThisFrame = false;
+
     public Transform leftPlayerSide;
     public Transform rightPlayerSide;
+    public Transform bottomPlayerSide;
+
+    bool pressingLeft;
+    bool pressingRight;
+    bool pressingUp;
+    bool pressingDown;
+
+    bool pressingJump;
+    bool pressingDash;
+
+
+    private void Update()
+    {
+        pressingLeft = Input.GetKey(KeyCode.LeftArrow);
+        pressingRight = Input.GetKey(KeyCode.RightArrow);
+        pressingUp = Input.GetKey(KeyCode.UpArrow);
+        pressingDown = Input.GetKey(KeyCode.DownArrow);
+
+        pressingJump = Input.GetKey(KeyCode.Z);
+        pressingDash = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKeyUp(KeyCode.Z) && isJumping)
+        {
+            isJumping = false;
+            reachedJumpApexThisFrame = true;
+        }
+    }
 
 
     private void FixedUpdate()
@@ -25,11 +57,7 @@ public class PlayerController : MonoBehaviour
         onLeftWall = Physics2D.BoxCast(leftPlayerSide.position, new Vector2(wallDetectionHorizontalDistance, wallDetectionVerticalDistance), 0, Vector2.left, 0, wallLayer);
         onRightWall = Physics2D.BoxCast(rightPlayerSide.position, new Vector2(wallDetectionHorizontalDistance, wallDetectionVerticalDistance), 0, Vector2.right, 0, wallLayer);
         onWall = onLeftWall || onRightWall;
-
-        bool pressingLeft = Input.GetKey(KeyCode.LeftArrow);
-        bool pressingRight = Input.GetKey(KeyCode.RightArrow);
-        bool pressingUp = Input.GetKey(KeyCode.UpArrow);
-        bool pressingDown = Input.GetKey(KeyCode.DownArrow);
+        onGround = Physics2D.Raycast(bottomPlayerSide.position, Vector2.down, 0.01f, wallLayer);   
 
         if (pressingLeft && !pressingRight && !onLeftWall)
         {
@@ -43,6 +71,13 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+
+        if(pressingJump && onGround && !isJumping)// could be onGround and jumoing, since the onGround check could be a bit too wide
+        {
+            isJumping = true;
+
+        }
+
 
 
     }
